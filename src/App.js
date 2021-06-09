e612eb44dea8c5307a5de82969088268df74e6cc
Kyle <kjfossman@gmail.com>
2021-06-09 06:23:52 -0800

    commit tree a71ef807974247e87a264b2f6ce6d4febfbd280e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { Component } from 'react';
+
+// import Timer from './Timer'
+// import Controls from './Controls'
+
+//no need to modify anything in this component
+class App extends Component {
+
+    state = {
+        peopleInSpace: []
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json)
+        .then(people => this.setState({ peopleInSpace: people }))
+    }
+    
+
+    render() {
+        return (
+          <div>
+            Testing
+          </div>
+        );
+      }
+}
+
+export default App
